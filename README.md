1afd9ca33b4f742f4c08384f9294a3b44896dc93
ephe-meral <j.appel.online@gmail.com>
2015-01-17 02:36:12 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Skill
+=====
+
+Defines skills. All of them. Skills? We gotz them.
---
+++ mix.exs
@@ -0,0 +1,31 @@
+defmodule Entice.Skill.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :entice_skill,
+     version: "0.0.1",
+     elixir: "~> 1.0",
+     deps: deps]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type `mix help compile.app` for more information
+  def application do
+    [applications: [:logger],
+     mod: {Entice.Skill, []}]
+  end
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type `mix help deps` for more examples and options
+  defp deps do
+    []
+  end
+end
